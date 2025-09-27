045162cc8b82d57259d77f68c8cafcda3caabccf
Aditya Sinha <iamaditya.sinha16@gmail.com>
2025-09-27 10:08:30 +0530

    ui enhancements

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,8 @@
 import { Socket } from "socket.io";
 import http from "http";
 
-import express from 'express';
-import { Server } from 'socket.io';
+import express from "express";
+import { Server } from "socket.io";
 import { UserManager } from "./managers/UserManger";
 
 const app = express();
@@ -10,21 +10,26 @@ const server = http.createServer(http);
 
 const io = new Server(server, {
   cors: {
-    origin: "*"
-  }
+    origin: "*",
+  },
 });
 
 const userManager = new UserManager();
 
-io.on('connection', (socket: Socket) => {
-  console.log('a user connected');
-  userManager.addUser("randomName", socket);
+io.on("connection", (socket: Socket) => {
+  console.log("a user connected");
+
+  // listen for "join" with the real name
+  socket.on("join", ({ name }) => {
+    userManager.addUser(name, socket);
+  });
+
   socket.on("disconnect", () => {
     console.log("user disconnected");
     userManager.removeUser(socket.id);
-  })
+  });
 });
 
 server.listen(3000, () => {
-    console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
